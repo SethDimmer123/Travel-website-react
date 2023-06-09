b438e3facaf50abb09eb4ff90e20e3480818a422
Seth Dimmer <sethdimmer1@gmail.com>
2023-06-09 12:22:42 -0400

    initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
 function Navbar() {
+    const [click,setClick] = useState(false)
+
+    const handleClick = () => setClick(!click);
   return (
   <>
   <nav className='navbar'>
@@ -11,7 +14,9 @@ function Navbar() {
             TRVL
             <i class='fab fa-typo3' />
         </Link>
-        
+        <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
     </div>
   </nav>
   </>
