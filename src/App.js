d79b07a8ce5ac5220548eb0c159ea1b718805ba8
Seth Dimmer <sethdimmer1@gmail.com>
2023-06-09 14:59:28 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 
+import { BrowserRouter as Router, Routes,Route } from 
 'react-router-dom';
 
 function App() {
   return (
     <>
     <Router>
+      <div className='App'>
     <Navbar/>
-    <Switch>
-      <Route path='/' exact />
-    </Switch>
+    <Routes>
+    <Route path='/' exact />
+    </Routes>
+      </div>
     </Router>
     
     </>
