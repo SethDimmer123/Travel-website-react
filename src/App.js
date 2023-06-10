c1c10ef53cec73be51e7a330af65360de1552aa4
Seth Dimmer <sethdimmer1@gmail.com>
2023-06-10 11:35:32 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 
 'react-router-dom';
+import Home from './components/pages/Home'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
     <Router>
     <Navbar/>
     <Switch>
-      <Route path='/' exact />
+      <Route path='/' exact component={Home} />
     </Switch>
     </Router>
 
